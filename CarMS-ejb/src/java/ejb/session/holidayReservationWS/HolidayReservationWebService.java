0392a1efc1ba20e25a0ca7ab4127d78f06912237
User <User@LAPTOP-G6LSP361>
2022-11-13 12:18:04 +0800

    Merge origin/master
    Conflicts:
    	CarMS-Library/src/ejb/session/stateless/ModelSessionBeanRemote.java
    	CarMS-Library/src/ejb/session/stateless/OutletSessionBeanRemote.java
    	CarMS-Library/src/exception/ModelNotFoundException.java
    	CarMS-ejb/src/java/ejb/session/stateless/ModelSessionBean.java
    	CarMS-ejb/src/java/ejb/session/stateless/ModelSessionBeanLocal.java
    	CarMS-ejb/src/java/ejb/session/stateless/OutletSessionBeanLocal.java
    	CarMS-ejb/src/java/ejb/session/stateless/RentalRateSessionBeanLocal.java

---
+++ CarMS-ejb/src/java/ejb/session/holidayReservationWS/HolidayReservationWebService.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejb.session.holidayReservationWS;
+
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author User
+ */
+@WebService(serviceName = "HotelReservationWebService")
+@Stateless()
+public class HolidayReservationWebService {
+
+    /**
+     * This is a sample web service operation
+     */
+    @WebMethod(operationName = "hello")
+    public String hello(@WebParam(name = "name") String txt) {
+        return "Hello " + txt + " !";
+    }
+}
